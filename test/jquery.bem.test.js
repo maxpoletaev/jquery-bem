77ba550314786e2559b72466100c7447f79d578c
Ian Clark <ian.clark.001@gmail.com>
2015-10-08 14:33:32 +0100

    Make using alternative BEM notation trivial
    I wanted to use `--` as the modifier notation rather than `_`,
    but found that because this the config was wrapped up in the
    constructor it was a little messy to do, and required the regexs
    to be recompiled.
    
    Therefore, I've moved the config setting into a separate
    `setConfig()` method, which is invoked during initialisation,
    but can be overriden by end-users:
    
    ```js
    $.BEM.setConfig({
    modPrefix: '--',
    modDlmtr: '--'
    });
    ```

--- test/jquery.bem.test.js
+++ test/jquery.bem.test.js
@@ -1,4 +1,12 @@
-QUnit.module('core');
+QUnit.module('core', {
+
+  /**
+   * Reset the config variables before each test case.
+   */
+  beforeEach: function() {
+    $.BEM.setConfig();
+  }
+});
 
 QUnit.test('get class type', function(assert) {
   assert.ok($.BEM.getClassType('block') == 'block',
@@ -26,6 +34,43 @@ QUnit.test('get class type', function(assert) {
   );
 
   assert.ok($.BEM.getClassType('other-class_unknown_elem__mod') == null,
+    'unknown class detection'
+  );
+});
+
+QUnit.test('overriding config', function(assert) {
+  $.BEM.setConfig({
+    namePattern: '[a-z]+',
+    elemPrefix: '____',
+    modPrefix: '--',
+    modDlmtr: '---'
+  });
+
+  assert.ok($.BEM.getClassType('block') == 'block',
+    'block class detection'
+  );
+
+  assert.ok($.BEM.getClassType('block____elem') == 'elem',
+    'element class detection'
+  );
+
+  assert.ok($.BEM.getClassType('block--key---val') == 'mod',
+    'block modifier class detection'
+  );
+
+  assert.ok($.BEM.getClassType('block____elem--key---val') == 'mod',
+    'element modifier class detection'
+  );
+
+  assert.ok($.BEM.getClassType('block--mod') == 'mod',
+    'boolean block modifier detection'
+  );
+
+  assert.ok($.BEM.getClassType('block____elem--mod') == 'mod',
+    'boolean element modifier detection'
+  );
+
+  assert.ok($.BEM.getClassType('other-class--unknown---elem__mod') == null,
     'unknown class detection'
   );
 });
